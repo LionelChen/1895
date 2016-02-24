14259d7c0f77f7ebe34f42e01038083115385588
LionelChen <chenlinchuan@me.com>
2016-02-24 10:12:14 +0800

    同步

--- client/demands/DemandDetail.js
+++ client/demands/DemandDetail.js
@@ -10,3 +10,44 @@ Template.DemandDetail.helpers({
         return Demand.findOne({_id:FlowRouter.getParam('_id')}).title;
     }
 });
+
+Template.DemandDetail.helpers({
+    demandDetailOwnerUsername: function () {
+        demandOwnerId = Demand.findOne({_id:FlowRouter.getParam('_id')}).owner;
+        return Meteor.users.findOne({_id:demandOwnerId}).username;
+    }
+});
+
+Template.DemandDetail.helpers({
+    demandDetailDateCreated: function () {
+        return Demand.findOne({_id:FlowRouter.getParam('_id')}).createdAt;
+    }
+});
+
+Template.DemandDetail.helpers({
+    demandDetailDescription: function () {
+        return Demand.findOne({_id:FlowRouter.getParam('_id')}).description;
+    }
+});
+
+Template.DemandDetail.helpers({
+    demandDetailExpectationExperience: function () {
+        return Demand.findOne({_id:FlowRouter.getParam('_id')}).expectation.experience;
+    }
+});
+
+Template.DemandDetail.helpers({
+    demandDetailExpectationPaid: function () {
+        if(Demand.findOne({_id:FlowRouter.getParam('_id')}).expectation.paid){
+            return "有偿"
+        }else{
+            return "无偿"
+        }
+        }
+});
+
+Template.DemandDetail.helpers({
+    demandDetailExpectationLocation: function () {
+        return Demand.findOne({_id:FlowRouter.getParam('_id')}).expectation.location;
+    }
+});
